52851991ff2d8f1d03faa0133cd208ddda034f5a
António Fagundes <antonio.fagundes@eclo.solutions>
2022-09-20 12:16:30 +0100

    Rework ReferenceVoltage enum to match Gecko SDK

--- nanoFramework.GiantGecko.Adc/ReferenceVoltage.cs
+++ nanoFramework.GiantGecko.Adc/ReferenceVoltage.cs
@@ -15,36 +15,71 @@ namespace nanoFramework.GiantGecko.Adc
         /// <summary>
         /// Internal 1.25 V reference.
         /// </summary>
-        Internal1_25V,
+        Internal1_25V = 0,
 
         /// <summary>
         /// Internal 2.5 V reference.
         /// </summary>
-        Internal2_5V,
+        Internal2_5V = 1,
 
         /// <summary>
         /// Buffered VDD.
         /// </summary>
-        BufferedVdd,
+        BufferedVdd = 2,
 
         /// <summary>
         /// Internal differential 5 V reference.
         /// </summary>
-        InternalDifferencial_5V,
+        InternalDifferencial_5V = 3,
 
         /// <summary>
         /// Single-ended external reference from pin 6.
         /// </summary>
-        SingleEndedExternalPin6,
+        SingleEndedExternalPin6 = 4,
 
         /// <summary>
         /// Differential external reference from pin 6 and 7.
         /// </summary>
-        DiffExternalPin6And7,
+        DiffExternalPin6And7 = 5,
 
         /// <summary>
         /// Unbuffered 2xVDD.
         /// </summary>
-        Unbuffered2Vdd,
+        Unbuffered2Vdd = 6,
+
+        /// <summary>
+        /// Internal Bandgap reference. Custom VFS.
+        /// </summary>
+        InternalBandgap = 128,
+
+        /// <summary>
+        /// Scaled AVDD: AVDD * VREFATT.
+        /// </summary>
+        ScaledAvdd = 129,
+
+        /// <summary>
+        /// Scaled singled ended external reference from pin 6: VREFP * VREFATT.
+        /// </summary>
+        ScaledSingleEndedExternalPin6 = 130,
+
+        /// <summary>
+        /// Raw single-ended external reference from pin 6. 
+        /// </summary>
+        RawSingleEndedExternalPin6 = 131,
+
+        /// <summary>
+        /// Special mode for entropy generation. 
+        /// </summary>
+        EntropyGeneration = 132,
+
+        /// <summary>
+        /// Scaled differential external Vref from pin 6 and 7: (VREFP - VREFN) * VREFATT.
+        /// </summary>
+        ScaledExternalPin6And7 = 133,
+
+        /// <summary>
+        /// Raw differential external Vref from pin 6 and 7: VREFP - VREFN.
+        /// </summary>
+        RawExternalPin6And7 = 134
     }
 }
